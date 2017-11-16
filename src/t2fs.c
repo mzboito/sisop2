2d1af85fdefc63ea49c0ae09fcf36bb2f28de4af
mzboito <mzboito@gmail.com>
2017-11-16 21:13:35 -0200

    some small fixes

--- src/t2fs.c
+++ src/t2fs.c
@@ -1,13 +1,12 @@
-
-
 #include "stdio.h"
 #include "stdlib.h"
 #include "ucontext.h"
 #include "../include/t2fs.h"
 #include "../include/apidisk.h"
 
-int identify2 (char *name, int size){
+int currentFileHandle = -1;
 
+int identify2(char *name, int size){
 	int length = 30;
 	  if(size < length){
 	      return -1;
@@ -23,13 +22,16 @@ int identify2 (char *name, int size){
 	return 0;
 }
 
-int createFileHandle () {
-	
+int createFileHandle(){
 	currentFileHandle++;
 	return currentFileHandle;
 }
 
 FILE2 create2 (char *filename) {
+	//what we need to create a file?
+
+	//
+
 
 	File_descriptor *fileDescr = malloc(sizeof(File_descriptor));
 	fileDescr->currentPointer = 0; //?
@@ -45,35 +47,22 @@ FILE2 create2 (char *filename) {
 	entradaDir->name = filename;
 	entradaDir->bytesFileSize = 0; //?
 
-	
+
 	fileDescr->registroDiretorio = registroDir; //?
 	fileDescr->entradaDiretorio = entradaDir; //?
 }
 
 int delete2 (char *filename);
-
 FILE2 open2 (char *filename);
-
 int close2 (FILE2 handle);
-
 int read2 (FILE2 handle, char *buffer, int size);
-
 int write2 (FILE2 handle, char *buffer, int size);
-
 int truncate2 (FILE2 handle);
-
 int seek2 (FILE2 handle, unsigned int offset);
-
 int mkdir2 (char *pathname);
-
 int rmdir2 (char *pathname);
-
 int chdir2 (char *pathname);
-
 int getcwd2 (char *pathname,int size);
-
 DIR2 opendir2 (char *pathname);
-
 int readdir2 (DIR2 handle,DIRENT2 *dentry);
-
 int closedir2 (DIR2 handle);
