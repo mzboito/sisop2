2e7333c9eed36296279be20ff2e76762609b8b4d
LisianeAguiar <Lisiane>
2017-11-21 22:35:11 -0200

    function isrelativepath

--- testes/findFreeCluster_tester.c
+++ testes/findFreeCluster_tester.c
@@ -9,15 +9,20 @@
 
 int main() {
   printf("Imprimindo valores das estruturas.\n");
-  
+  char names[] = {'.','i','s','i','a','n','e',':','2','5','2','7','3','9','\n',
+		          'M','a','r','c','e','l','y',':','2','2','8','4','5','4'};
  
   if (structures_init() == 0) {
 	printf("\n\n");
 	printf("Find free cluster return: %08x\n", findFreeCluster());
 	printf("\n\n");
-	printf("Mark cluster ocupado: %08x\n", markCluster());
+	printf("Mark cluster ocupado: %08x\n", set_cluster(4));
 	printf("\n\n");
-	printf("Free cluster: %08x\n", freeCluster());
+	printf("Free cluster: %08x\n", free_cluster(3));
+printf("\n\n");
+printf("\n\n");
+printf("Is Relative: ", isRelativePath (names));
+
 
   }
   return 0;
